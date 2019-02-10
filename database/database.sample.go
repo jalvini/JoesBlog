799fa0332b8b24ea84809c5c8fe069c460ca2280
jalvini <Makeda@2300>
2019-02-09 20:51:32 -0500

    Changed .gitignore And Added Sample Database File

---
+++ database/database.sample.go
@@ -0,0 +1,23 @@
+package database
+
+/* Rename This File To database.go and Change Vars To Match Your DB Setu
+import (
+	"database/sql"
+	"log"
+)
+
+var DB string = "****"
+var DBUSER string= "****"
+var DBPASSWORD string = "****"
+var DBHOST string = "localhost"
+var DBPORT string = "3306"
+
+func DbInit() (*sql.DB){
+	db, err := sql.Open("mysql", DBUSER  + ":" + DBPASSWORD + "@tcp(" + DBHOST + ":" + DBPORT + ")/" + DB)
+
+	if err != nil {
+		log.Print(err.Error())
+	}
+	return db
+}
+*/
